fe22c5bf061b295370c2712b089f254cf84ab06f
luckyyadav <yadav.lucky1990@gmail.com>
2023-03-15 15:33:35 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {
   ViewChild,
 } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -19,18 +19,23 @@ export class AppComponent implements OnInit, AfterViewInit {
   private ctx: CanvasRenderingContext2D;
   reader: FileReader = new FileReader();
   imgPath: any = new Image();
+  onChangeStarted = false;
   photos;
   constructor(private http: HttpClient) {}
 
-  ngOnInit(){
-  this.getPhotos()
+  ngOnInit() {
+    setTimeout(() => {
+      this.getPhotos();
+    }, 1000);
   }
 
-  getPhotos(){
-    this.http.get("https://picsum.photos/v2/list?page=2&limit=4").subscribe((res)=>{
-      this.photos = res;
-      console.log(res)
-    })
+  getPhotos() {
+    this.http
+      .get('https://picsum.photos/v2/list?page=2&limit=4')
+      .subscribe((res) => {
+        this.photos = res;
+        console.log(res);
+      });
   }
 
   ngAfterViewInit() {
@@ -38,18 +43,44 @@ export class AppComponent implements OnInit, AfterViewInit {
   }
 
   UploadFile(file) {
-    this.reader.readAsDataURL(file.target.files[0]);
-    this.readFiles();
+    console.log(file.target.files[0]);
+    this.onChangeStarted = true;
+    this.readFiles(file);
   }
 
-  readFiles() {
-    this.reader.onload = () => {
-      console.log(this.reader.result);
-      this.imgPath['src'] = this.reader.result;
-      
+  readFiles(file?) {
+    if (this.onChangeStarted) {
+      this.reader.readAsDataURL(file.target.files[0]);
+      this.reader.onload = () => {
+        console.log(this.reader.result);
+        this.imgPath['src'] = this.reader.result;
         console.log(this.imgPath);
         this.ctx.drawImage(this.imgPath, 0, 0);
-    
-    };
+      };
+    } else {
+      this.ctx.drawImage(this.imgPath, 0, 0);
+    }
+  }
+
+  // drag drop
+
+  allowDrop(ev) {
+    this.onChangeStarted = false;
+    ev.preventDefault();
+    console.log(ev.dataTransfer);
+  }
+
+  drag(ev, obj) {
+    console.log(ev);
+    ev.dataTransfer.setData('foo', JSON.stringify(obj));
+    // var t = JSON.stringify(obj)
+    const dataTransfer = new DataTransfer();
+    dataTransfer.items.add(obj.download_url);
+  }
+
+  drop(ev) {
+    ev.preventDefault();
+    console.log('drop', ev);
+    this.readFiles();
   }
 }
